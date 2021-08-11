7bf534608155c568e91f08e853b1f834a5b60b49
cris.dux <crisfer.4217@gmail.com>
2021-08-10 21:17:27 -0400

    Todo el proyecto con la configuracion de base

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container:{
+      center:true,
+      padding:'2rem'
+    },
+    fontFamily:{
+      'poppins':['Poppins, sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
